f1fd43ef9b7f4b97cebd7c54add9b7a4863ff246
Bosh@Minifridge <ohwaitnvm at gmail>
2013-08-14 21:20:44 -0700

    rails new

---
+++ src/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Matchbrick::Application.config.secret_token = '099ec9bef4f01559c92709945598917edc57c3fb45eac2ec86c7176be0331bb871bedbdd7c7f4b6465bd6c5cfea95da1b0a64156c829abf42e552bed7e520299'
